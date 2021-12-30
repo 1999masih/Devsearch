971210ba00e6f59cbad277c5ff7fffe675c80ef8
meshikhah <masihsaeedi.s@gmail.com>
2021-12-30 14:26:17 +0330

    User edit profile

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 # Create your views here.
 
 
@@ -53,7 +53,7 @@ def register_user(request):
             messages.success(request, 'User account was created!')
 
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
         else:
             messages.error(request, 'An error occured during registiration!')
 
@@ -83,4 +83,18 @@ def user_account(request):
     # other_skills = profile.skill_set.filter(description="")
 
     context = {'profile':profile, 'skills':skills, 'projects':projects}
-    return render(request, 'users/account.html', context) 
+    return render(request, 'users/account.html', context)
+
+@login_required(login_url='login')
+def edit_account(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+    context = {'form':form}
+    return render(request, 'users/profile_form.html', context)
