a1a09c6a42b75bf6c347b22f13dc2bc265aadc99
meshikhah <masihsaeedi.s@gmail.com>
2022-01-02 16:23:33 +0330

    pagination now works but search needs a fix

--- projects/views.py
+++ projects/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 from .models import Project, Tag
 from .forms import ProjectForm
-from .utils import search_projects
+from .utils import search_projects, paginate_projects
 
 
 def projects(request):
-    
     projects, search_query = search_projects(request)
 
-    context = {'projects': projects, 'search_query':search_query}
+    custom_range, projects = paginate_projects(request, projects, 3)
+
+    context = {'projects': projects, 'search_query':search_query, 'custom_range':custom_range}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
