c239ccac9313070c72bae6cf3219fd7e7708a17b
meshikhah <masihsaeedi.s@gmail.com>
2022-01-04 19:30:25 +0330

    Sending message to other users is now working

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile, Skill
+from .models import Profile, Skill, Message
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
@@ -47,6 +47,21 @@ class SkillForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(SkillForm, self).__init__(*args, **kwargs)
+
+        # self.fields['title'].widget.attrs.update({'class': 'input',})#css class
+        # self.fields['description'].widget.attrs.update({'class': 'input'})
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'email', 'subject', 'body']
+
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
 
         # self.fields['title'].widget.attrs.update({'class': 'input',})#css class
         # self.fields['description'].widget.attrs.update({'class': 'input'})
